5099afd8fb82090b8c29d582f6f1ca8555a0586b
Kieran Everett <kieraneverett03@gmail.com>
2022-10-25 10:24:33 +0100

    Created boilerplate code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("a")
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+
+function preload() {
+
+};
+
+function create() {
+
+};
+
+function update() {
+
+};
