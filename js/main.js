ea11c8df78ce288fbf1fbfc45786b3d27b69759d
Kieran Everett <kieraneverett03@gmail.com>
2022-10-26 22:00:39 +0100

    Adding comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Phaser config
 let config = {
     type: Phaser.AUTO,
     width: 600,
@@ -12,7 +13,7 @@ let config = {
     }
 };
 
-
+// Defining global variables
 let player;
 let cursors;
 let fire;
@@ -33,56 +34,70 @@ let enemyStep = 0;
 let lastEnemyStep = enemyStep;
 let lastStepTime = currentTime;
 
+// Creating game object
 let game = new Phaser.Game(config);
 
 
 function preload() {
+    // Loading assets
     this.load.image('character', 'assets/player.png');
     this.load.image('playerBullet', 'assets/playerBullet.png');
     this.load.image('enemy', 'assets/enemy.png');
 }
 
 function create() {
+    // Setting up player physics
     player = this.physics.add.sprite(config.width / 2, 450, 'character');
     player.setCollideWorldBounds(true);
 
+    // Variables for getting player inputs
     cursors = this.input.keyboard.createCursorKeys();
     fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
 
+    // Creating groups for bullets
     playerBullets = this.physics.add.group();
     enemyBullets = this.physics.add.group();
 
-
+    // Enemy physics
     enemy = this.physics.add.sprite(config.width / 2, 200, 'enemy');
 
+    // Displaying player HP text
     playerHPText = this.add.text(10, 750, 'HP: ' + playerHP, { fontSize: '32px', fill: '#FFFFFF'});
 
 
-    this.physics.add.overlap(enemy, playerBullets, damageEnemy, null, this);
-    this.physics.add.overlap(player, enemyBullets, damagePlayer, null, this);
+    // Collision
+    this.physics.add.overlap(enemy, playerBullets, damageEnemy, null, this); // Enemy getting hit by playerBullets
+    this.physics.add.overlap(player, enemyBullets, damagePlayer, null, this); // Player getting hit by enemyBullets
 }
 
 function update() {
 
+    // If gameOver then don't run update
     if (gameOver) return;
 
+    // Getting the current time for things that need delta time
     currentTime = new Date();
 
+    // Calculating current step for the enemy AI
+    // Each step lasts a number of miliseconds and then it increments the step count for controling certain behaviours like when the enemy fires
     if (currentTime - lastStepTime > 500) {
         enemyStep += 1;
-        lastStepTime = currentTime;
+        lastStepTime = currentTime; // Updating lastStepTime so it knows how long as been since the last step took place
     }
 
     
+    // Making the player's bullets wiggle by iterating over and applying a sine to their X velocity based on their Y position
     playerBullets.children.iterate(function (child) {
         child.setVelocityX(Math.sin(child.y / 25) * 200);
     });
 
+    // If this is a new step then run this
     if (enemyStep > lastEnemyStep){
 
-        if (enemyState == 1) {
-            fireEnemyBullet(Math.random() * config.width + 1, 0, 0, 500);
-        } else if (enemyState == 2) {
+        // State machine stuff
+        if (enemyState == 1) { // Random bullets falling from the top of the screen
+            fireEnemyBullet(Math.random() * config.width + 1, 0, 0);
+        } else if (enemyState == 2) { // Bullets are targeted to the player and start at the enemy
             // TODO Normalize the velocity vector
             
             /*
@@ -94,76 +109,89 @@ function update() {
             bulletVectorX *= 100;
             bulletVectorY *= 100;
 
-            fireEnemyBullet(enemy.x, enemy.y, bulletVectorX, bulletVectorY, 500);
+            fireEnemyBullet(enemy.x, enemy.y, bulletVectorX, bulletVectorY;
             */
             
-            fireEnemyBullet(enemy.x, enemy.y, (enemy.x - player.x) * -1, (enemy.y - player.y) * -1, 500);
+            fireEnemyBullet(enemy.x, enemy.y, (enemy.x - player.x) * -1, (enemy.y - player.y) * -1);
         }
 
     }
 
 
+    // If player is pressing the fire button and it has been a certain amount of time since the last bullet was fired then fire a bullet
     if (fire.isDown && (currentTime - lastBulletFire) > 200) {
         firePlayerBullet();
-        lastBulletFire = new Date();
+        lastBulletFire = new Date(); // Save when this was so it knows how long it has been since the last bullet was fired
     }
 
 
-    if (cursors.left.isDown) {
+    // Player movement
+    if (cursors.left.isDown) { // Left
         player.setVelocityX(-200);
-    } else if (cursors.right.isDown) {
+    } else if (cursors.right.isDown) { // Right
         player.setVelocityX(200);
-    } else {
+    } else { // Neutral
         player.setVelocityX(0);
     }
 
-    if (cursors.down.isDown) {
+    if (cursors.down.isDown) { // Down
         player.setVelocityY(200);
-    } else if (cursors.up.isDown) {
+    } else if (cursors.up.isDown) { // Up
         player.setVelocityY(-200);
-    } else {
+    } else { // Neutral
         player.setVelocityY(0);
     }
 
+    // Updating the current step
     lastEnemyStep = enemyStep;
 }
 
 
+// Player bullet firing function
 function firePlayerBullet() {
+    // Making the bullet
     let bullet = playerBullets.create(player.x, player.y, 'playerBullet');
 
     //bullet.setCollideWorldBounds(true);
+
+    // Setting its velocity
     bullet.setVelocity(0, -300);
 }
 
+// Enemy bullet firing function
 function fireEnemyBullet(x, y, velocityX, velocityY) {
+    // Making the bullet
     let bullet = enemyBullets.create(x, y, 'playerBullet');
 
+    // Setting its velocity based on arguments
     bullet.setVelocity(velocityX, velocityY);
 }
 
+// Damaging enemy function
 function damageEnemy(enemy, bullet) {
-    enemyHP -= 1;
+    enemyHP -= 1; // Dealing damage
     console.log(enemyHP);
-    bullet.destroy();
+    bullet.destroy(); // Destroying the playerBullet that hit the enemy
 
-    if (enemyHP == 0) {
+    if (enemyHP == 0) { // Game over if enemy has 0 HP
         this.physics.pause();
         gameOver = true;
     }
 }
 
+// Damaging the player function
 function damagePlayer(player, bullet) {
-    playerHP -= 1;
-    playerHPText.setText('HP: ' + playerHP);
-    bullet.destroy();
+    playerHP -= 1; // Dealing damage
+    playerHPText.setText('HP: ' + playerHP); // Updating the UI with the current player HP
+    bullet.destroy(); // Destroying the enemy bullet
 
-    if (playerHP == 0) {
+    if (playerHP == 0) { // Game over if the player has 0 HP
         this.physics.pause();
         gameOver = true;
     }
 }
 
+/*
 function vectorMagnitude(x, y) {
     return Math.sqrt((x * x) + (y * y));
 }
@@ -180,3 +208,4 @@ function vectorNormalize(x, y) {
         return [0, 0];
     }
 }
+*/
